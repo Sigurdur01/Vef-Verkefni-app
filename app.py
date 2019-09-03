4ad9479899abc784d392265a209e34e3ac23b403
Sigurdur01 <33860834+Sigurdur01@users.noreply.github.com>
2019-09-03 20:29:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
 #Sigurður Ingi Brynjarsson
-
 from flask import Flask,render_template,redirect
 from sys import argv
-
 app = Flask(__name__)
-
 hrefs = {
             "href main":"/",
             "href sida2":"/sidur/2",
             "href sida3":"/sidur/3"
         }
-
 linktext = {
                 "Hlekur1":"Heim",
                 "Hlekur2":"Sida2",
                 "Hlekur3":"Sida3"
             }
-
 contents = {
                 "content":"Hello Universe",
 		"Hlekur1":linktext["Heim"],
@@ -26,8 +21,7 @@ contents = {
                 "href2":hrefs["href sida2"],
                 "href3":hrefs["href sida3"]
 		}
-
-pg2contents = {
+sida2contents = {
                 "content":"Þetta er Sida 2",
 		"Hlekur1":linktext["Heim"],
                 "Hlekur2":linktext["Hlekur2"],
@@ -36,8 +30,7 @@ pg2contents = {
                 "href2":hrefs["href sida2"],
                 "href3":hrefs["href sida3"]
                }
-
-pg3contents = {
+sida3contents = {
                 "content":"Þetta er Sida 3",
 		"Hlekur1":linktext["Heim"],
                 "Hlekur2":linktext["Hlekur2"],
@@ -46,20 +39,16 @@ pg3contents = {
                 "href2":hrefs["href sida2"],
                 "href3":hrefs["href sida3"]
                }
-
 @app.route("/")
 def index():
     return render_template('head.html', cnt=contents)
-
-
-@app.route("/sidur/<int:pgnumber>", methods=['Get'])
-def page(pgnumber):
-    if pgnumber == 1:
+@app.route("/sidur/<int:sidunumber>", methods=['Get'])
+def sida(sidunumber):
+    if sidunumber == 1:
         return redirect("/")
-    elif pgnumber == 2:
-        return render_template('head.html', cnt=pg2contents)
-    elif pgnumber == 3:
-        return render_template('head.html', cnt=pg3contents)
-
+    elif sidunumber == 2:
+        return render_template('head.html', cnt=sidu2contents)
+    elif sidunumber == 3:
+        return render_template('head.html', cnt=sidu3contents)
 if (__name__) == '__main__':
     app.run(debug=True)
