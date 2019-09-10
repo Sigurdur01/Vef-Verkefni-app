5eed19a59cf01f241ea975d294dc9bea3aefa7c7
Sigurdur01 <33860834+Sigurdur01@users.noreply.github.com>
2019-09-10 16:54:22 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,55 +1,22 @@
-  
 #Sigurður Ingi Brynjarsson
-from flask import Flask,render_template,redirect
+from flask import Flask
 from sys import argv
 app = Flask(__name__)
-hrefs = {
-    "href main":"/",
-    "href sida2":"/sidur/2",
-    "href sida3":"/sidur/3"
-}
-linktext = {
-    "Hlekur1":"Heim",
-    "Hlekur2":"Sida2",
-    "Hlekur3":"Sida3"
-}
-contents = {
-    "content":"Hello Universe",
-    "Hlekur1":linktext["Heim"],
-    "Hlekur2":linktext["Hlekur2"],
-    "Hlekur3":linktext["Hlekur3"],
-    "href1":hrefs["href main"],
-    "href2":hrefs["href sida2"],
-    "href3":hrefs["href sida3"]
-}
-sida2contents = {
-    "content":"detta er Sida 2",
-    "Hlekur1":linktext["Heim"],
-    "Hlekur2":linktext["Hlekur2"],
-    "Hlekur3":linktext["Hlekur3"],
-    "href1":hrefs["href main"],
-    "href2":hrefs["href sida2"],
-    "href3":hrefs["href sida3"]
-}
-sida3contents = {
-    "content":"detta er Sida 3",
-    "Hlekur1":linktext["Heim"],
-    "Hlekur2":linktext["Hlekur2"],
-    "Hlekur3":linktext["Hlekur3"],
-    "href1":hrefs["href main"],
-    "href2":hrefs["href sida2"],
-    "href3":hrefs["href sida3"]
-}
 @app.route("/")
 def index():
-    return render_template('head.html', cnt=contents)
-@app.route("/sidur/<int:sidunumber>", methods=['Get', 'POST'])
-def sida(sidunumber):
-    if sidunumber == 1:
-        return redirect("/")
-    elif sidunumber == 2:
-        return render_template('head.html', cnt=sidu2contents)
-    elif sidunumber == 3:
-        return render_template('head.html', cnt=sidu3contents)
+    return (	<li><a href='/'> 'Home' </a></li>
+				<li><a href='/sida2'> 'Sida2' </a></li>
+				<li><a href='/sida3'> 'Sida3' </a></li>
+)
+@app.route("/sida2")
+    return (	<li><a href='/'> 'Home' </a></li>
+				<li><a href='/sida2'> 'Sida2' </a></li>
+				<li><a href='/sida3'> 'Sida3' </a></li>
+)
+@app.route("/sida3")
+    return (	<li><a href='/'> 'Home' </a></li>
+				<li><a href='/sida2'> 'Sida2' </a></li>
+				<li><a href='/sida3'> 'Sida3' </a></li>
+)
 if (__name__) == '__main__':
     app.run(debug=True)
