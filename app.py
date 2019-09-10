59e2a8633f536f7578696bd4663324e37a1ffb52
Sigurdur01 <33860834+Sigurdur01@users.noreply.github.com>
2019-09-10 16:09:23 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,44 +1,45 @@
+  
 #Sigurður Ingi Brynjarsson
 from flask import Flask,render_template,redirect
 from sys import argv
 app = Flask(__name__)
 hrefs = {
-       "href main":"/",
-       "href sida2":"/sidur/2",
-       "href sida3":"/sidur/3"
-        }
+    "href main":"/",
+    "href sida2":"/sidur/2",
+    "href sida3":"/sidur/3"
+}
 linktext = {
-            "Hlekur1":"Heim",
-            "Hlekur2":"Sida2",
-            "Hlekur3":"Sida3"
-            }
+    "Hlekur1":"Heim",
+    "Hlekur2":"Sida2",
+    "Hlekur3":"Sida3"
+}
 contents = {
-            "content":"Hello Universe",
-	    "Hlekur1":linktext["Heim"],
-            "Hlekur2":linktext["Hlekur2"],
-            "Hlekur3":linktext["Hlekur3"],
-            "href1":hrefs["href main"],
-            "href2":hrefs["href sida2"],
-            "href3":hrefs["href sida3"]
-		}
+    "content":"Hello Universe",
+    "Hlekur1":linktext["Heim"],
+    "Hlekur2":linktext["Hlekur2"],
+    "Hlekur3":linktext["Hlekur3"],
+    "href1":hrefs["href main"],
+    "href2":hrefs["href sida2"],
+    "href3":hrefs["href sida3"]
+}
 sida2contents = {
-                "content":"detta er Sida 2",
-		"Hlekur1":linktext["Heim"],
-                "Hlekur2":linktext["Hlekur2"],
-                "Hlekur3":linktext["Hlekur3"],
-		"href1":hrefs["href main"],
-                "href2":hrefs["href sida2"],
-                "href3":hrefs["href sida3"]
-               }
+    "content":"detta er Sida 2",
+    "Hlekur1":linktext["Heim"],
+    "Hlekur2":linktext["Hlekur2"],
+    "Hlekur3":linktext["Hlekur3"],
+    "href1":hrefs["href main"],
+    "href2":hrefs["href sida2"],
+    "href3":hrefs["href sida3"]
+}
 sida3contents = {
-                "content":"detta er Sida 3",
-		"Hlekur1":linktext["Heim"],
-                "Hlekur2":linktext["Hlekur2"],
-                "Hlekur3":linktext["Hlekur3"],
-		"href1":hrefs["href main"],
-                "href2":hrefs["href sida2"],
-                "href3":hrefs["href sida3"]
-               }
+    "content":"detta er Sida 3",
+    "Hlekur1":linktext["Heim"],
+    "Hlekur2":linktext["Hlekur2"],
+    "Hlekur3":linktext["Hlekur3"],
+    "href1":hrefs["href main"],
+    "href2":hrefs["href sida2"],
+    "href3":hrefs["href sida3"]
+}
 @app.route("/")
 def index():
     return render_template('head.html', cnt=contents)
