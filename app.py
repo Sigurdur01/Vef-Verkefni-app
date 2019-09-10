1b4f1c405e5cd79209f7a77867140915a2930391
Sigurdur01 <33860834+Sigurdur01@users.noreply.github.com>
2019-09-10 16:58:47 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -4,19 +4,16 @@ from sys import argv
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return (	<li><a href='/'> 'Home' </a></li>
-				<li><a href='/sida2'> 'Sida2' </a></li>
-				<li><a href='/sida3'> 'Sida3' </a></li>
-)
+    return '<li><a href='/'> 'Home' </a> </li><li><a href='/sida2'> 'Sida2' </a> </li><li><a href='/sida3'> 'Sida3' </a></li>'
+
 @app.route("/sida2")
-    return (	<li><a href='/'> 'Home' </a></li>
-				<li><a href='/sida2'> 'Sida2' </a></li>
-				<li><a href='/sida3'> 'Sida3' </a></li>
-)
+def sida2():
+    return '<li><a href='/'> 'Home' </a> </li><li><a href='/sida2'> 'Sida2' </a> </li><li><a href='/sida3'> 'Sida3' </a></li>'
+
 @app.route("/sida3")
-    return (	<li><a href='/'> 'Home' </a></li>
-				<li><a href='/sida2'> 'Sida2' </a></li>
-				<li><a href='/sida3'> 'Sida3' </a></li>
-)
+def sida3():
+    return '<li><a href='/'> 'Home' </a> </li><li><a href='/sida2'> 'Sida2' </a> </li><li><a href='/sida3'> 'Sida3' </a></li>'
+
+
 if (__name__) == '__main__':
     app.run(debug=True)
