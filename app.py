74cbb00a832766345111aeb05fbe7cbed588017f
Sigurdur01 <33860834+Sigurdur01@users.noreply.github.com>
2019-09-10 17:01:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 #Sigurður Ingi Brynjarsson
 from flask import Flask
-from sys import argv
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return '<li><a href='/'> 'Home' </a> </li><li><a href='/sida2'> 'Sida2' </a> </li><li><a href='/sida3'> 'Sida3' </a></li>'
+    return '<li><a href='/'> Home </a> </li><li><a href='/sida2'> Sida2 </a> </li><li><a href='/sida3'> Sida3 </a></li>'
 
 @app.route("/sida2")
 def sida2():
-    return '<li><a href='/'> 'Home' </a> </li><li><a href='/sida2'> 'Sida2' </a> </li><li><a href='/sida3'> 'Sida3' </a></li>'
+    return '<li><a href='/'> Home </a> </li><li><a href='/sida2'> Sida2 </a> </li><li><a href='/sida3'> Sida3 </a></li>'
 
 @app.route("/sida3")
 def sida3():
-    return '<li><a href='/'> 'Home' </a> </li><li><a href='/sida2'> 'Sida2' </a> </li><li><a href='/sida3'> 'Sida3' </a></li>'
+    return '<li><a href='/'> Home </a> </li><li><a href='/sida2'> Sida2 </a> </li><li><a href='/sida3'> Sida3 </a></li>'
 
 
-if (__name__) == '__main__':
+if __name__== '__main__':
     app.run(debug=True)
